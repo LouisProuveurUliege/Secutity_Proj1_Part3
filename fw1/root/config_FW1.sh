ab8234d5f4496538de6fb4072cc4c61e1f245fbd
LouisProuveurUliege <louis.prouveur@student.uliege.be>
2024-11-17 12:24:03 +0100

    done rules for fw1

--- fw1/root/config_FW1.sh
+++ fw1/root/config_FW1.sh
@@ -1,1 +1,40 @@
 ###### Rules for FW1 ######
+
+# Incoming traffic z-mail-ssh
+iptables -A FORWARD -d 172.31.6.5 -p tcp --dport 993 -j ACCEPT
+iptables -A FORWARD -d 172.31.6.5 -p tcp --dport 25 -j ACCEPT
+iptables -A FORWARD -d 172.31.6.6 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -d 172.31.6.0/24 -j DROP
+
+# Outgoing traffic z-mail-ssh
+iptables -A FORWARD -s 173.31.6.6 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -s 173.31.6.5 -p tcp --dport 25 -j ACCEPT
+iptables -A FORWARD -s 172.31.6.0/24 -j DROP
+
+# Incoming traffic z-http
+iptables -A FORWARD -d 172.31.5.0/24 -j DROP
+
+# Outgoing traffic z-http 
+iptables -A FORWARD -s 172.31.5.3 -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -s 172.31.5.3 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -s 172.31.5.4 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -s 172.31.5.4 -p tcp --dport 443 -j ACCEPT
+iptables -A FORWARD -s 172.31.5.0/24 -j DROP
+
+# Incoming traffic z-public
+iptables -A FORWARD -d 172.32.5.2 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -d 172.32.5.2 -p tcp --dport 443 -j ACCEPT
+iptables -A FORWARD -s 172.31.6.6 -d 172.32.5.2 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -d 172.32.5.3 -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -d 172.32.5.3 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -d 172.32.5.0/24 -j DROP
+
+# Outgoing traffic z-public
+iptables -A FORWARD -s 172.32.5.3 -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -s 172.32.5.3 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -s 172.32.5.0/24 -j DROP
+
+# Deny by default
+iptables -A FORWARD -j LOG --log-prefix "firewall 1"
+iptables -A FORWARD -j DROP
+
